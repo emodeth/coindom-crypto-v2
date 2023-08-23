fd33ab0f07a0bf1f8742a7e7d30ee679a079b998
emodeth <emodethistaken@gmail.com>
2023-08-23 17:31:18 +0300

    Initial commit

---
+++ src/pages/coin/Coin.jsx
@@ -0,0 +1,7 @@
+import "./coin.scss";
+
+function c() {
+  return <div></div>;
+}
+
+export default c;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,7 @@
+import "./home.scss";
+
+function home() {
+  return <div></div>;
+}
+
+export default home;

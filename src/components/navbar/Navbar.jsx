363537b24a7a630f33f14bf7c41df6424fe227ea
emodeth <emodethistaken@gmail.com>
2023-08-28 15:40:49 +0300

    Sticky navbar added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react";
 import "./navbar.scss";
 
 function Navbar({ isOpen, setIsOpen }) {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(function () {
+    window.addEventListener("scroll", () => {
+      window.scrollY >= 160 ? setIsSticky(true) : setIsSticky(false);
+    });
+  }, []);
+
   return (
-    <nav>
+    <nav className={isSticky ? "sticky-nav" : ""}>
       <div className="navbar">
         <h2 className="navbar__logo">Coindom</h2>
         <ul className="navbar__nav">
